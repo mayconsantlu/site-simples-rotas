2b3a24849ed007d6f5963b47d3a637054a2dd5f9
Maycon Luczynski <mayconsantlu@gmail.com>
2014-07-18 17:24:56 -0300

    Inserido mais dados nas páginas

--- produtos.php
+++ produtos.php
@@ -21,4 +21,22 @@
 						</div>
 					</div>
 				</div>
+<div class="media-body">
+					<h4 class="media-heading">
+						Terceiro Produto
+					</h4> 
+					<p class="text-justify">Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra l&aacute; , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. M&eacute; faiz elementum girarzis, nisi eros vermeio, in elementis m&eacute; pra quem &eacute; amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.
+					</p>
+					<div class="media">
+						 <a href="#" class="pull-left"><img src="http://lorempixel.com/64/64/" class="media-object" alt='' /></a>
+						<div class="media-body">
+							<h4 class="media-heading">
+								Quarto produto
+							</h4>
+							<p class="text-justify">Mussum ipsum cacilds, vidis litro abertis. Consetis adipiscings elitis. Pra l&aacute; , depois divoltis porris, paradis. Paisis, filhis, espiritis santis. M&eacute; faiz elementum girarzis, nisi eros vermeio, in elementis m&eacute; pra quem &eacute; amistosis quis leo. Manduma pindureta quium dia nois paga. Sapien in monti palavris qui num significa nadis i pareci latim. Interessantiss quisso pudia ce receita de bolis, mais bolis eu num gostis.
+							</p>
+						</div>
+					</div>
+				</div>
+				
 			</div>
