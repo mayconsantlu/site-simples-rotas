fc1e49629d54c5b2509abc89687261838f194ba8
Maycon Luczynski <mayconsantlu@gmail.com>
2014-07-22 10:10:13 -0300

    Ajustado link do breadcump

--- includes/voce-esta.php
+++ includes/voce-esta.php
@@ -1,6 +1,6 @@
 <ul class="breadcrumb">
     <li>
-        Voc&ecirc; est&aacute; em: <a href="index.php">Home</a>
+        Voc&ecirc; est&aacute; em: <a href="/">Home</a>
     </li>
     <li>
         <?php echo $titulo?>
